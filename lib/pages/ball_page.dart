d0c666260b856fdae2f09505089ded10a4638ea7
Oyebliss <oyebisi695@gmail.com>
2024-04-02 12:18:50 +0100

    First commit

---
+++ lib/pages/ball_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BallPage extends StatefulWidget {
+  const BallPage({super.key});
+
+  @override
+  State<BallPage> createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
